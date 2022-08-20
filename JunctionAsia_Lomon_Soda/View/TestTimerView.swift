6d3adbc541bce38d521fbf3592870e4407a270ec
taek0622 <min4644@gmail.com>
2022-08-21 04:51:41 +0900

    미뉴가 레퍼런스 추가함

--- JunctionAsia_Lomon_Soda/View/TestTimerView.swift
+++ JunctionAsia_Lomon_Soda/View/TestTimerView.swift
@@ -65,3 +65,11 @@ public struct StoppableAnimationModifier<Value: VectorArithmetic>: AnimatableMod
     }
 }
 
+// https://stackoverflow.com/questions/67509473/how-to-stop-animation-in-the-current-state-in-swiftui
+// https://stackoverflow.com/questions/58041676/how-to-pause-an-animation-in-swiftui
+// https://developer.apple.com/forums/thread/129076
+// https://stackoverflow.com/questions/69663197/how-can-i-reverse-the-slide-transition-for-a-swiftui-animation
+// https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-and-remove-views-with-a-transition
+// https://developer.apple.com/documentation/swiftui/animation
+// https://smashswift.com/how-to-change-animation-duration-and-delay-in-swiftui/
+// https://seons-dev.tistory.com/entry/SwiftUI-%EC%95%A0%EB%8B%88%EB%A9%94%EC%9D%B4%EC%85%98%EA%B3%BC-%EB%B9%84%EC%8A%B7%ED%95%9C-Transitions
