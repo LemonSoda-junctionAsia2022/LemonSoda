bfdaa48b0a2b720245b0cfb2beab3e8fcc3b4b51
Rookie0031 <jisu7408@gmail.com>
2022-08-21 00:17:16 +0900

    fix: 복구작업

---
+++ JunctionAsia_Lomon_Soda/ViewModel/LoadData.swift
@@ -0,0 +1,30 @@
+//
+//  test3.swift
+//  JunctionAsia_Lomon_Soda
+//
+//  Created by Jisu Jang on 2022/08/20.
+//
+import Foundation
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+        print(data)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
