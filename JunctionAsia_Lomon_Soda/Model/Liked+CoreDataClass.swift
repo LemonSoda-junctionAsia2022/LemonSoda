421e03e87f306da59af45a3b9e9bb8c42ec9ef43
taek0622 <min4644@gmail.com>
2022-08-21 05:58:48 +0900

    코어데이터 매니저 추가

---
+++ JunctionAsia_Lomon_Soda/Model/Liked+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Liked+CoreDataClass.swift
+//  JunctionAsia_Lomon_Soda
+//
+//  Created by 김민택 on 2022/08/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Liked)
+public class Liked: NSManagedObject {
+
+}
