a75778b52012862525431105f5917a22d03d4d62
Juny0110 <wnsdyds403@naver.com>
2022-08-20 18:15:49 +0900

    wip: popupView 진행중

--- JunctionAsia_Lomon_Soda/View/popupView.swift
+++ JunctionAsia_Lomon_Soda/View/popupView.swift
@@ -10,38 +10,39 @@ import SwiftUI
 struct popupView: View {
     
     var body: some View {
-        
         ZStack{
-            Color.black.ignoresSafeArea().opacity(0.5)
-        VStack{
-            ScrollView(showsIndicators: false) {
-                HStack {
-                    Text("좋아하는 작품 모음")
-                        .font(.system(size: 22, weight: .bold))
-                    Spacer()
-                }
-                
-                ForEach(0..<4) {_ in
-                    HStack(spacing: 40) {
+            Color.black
+                .ignoresSafeArea()
+                .opacity(0.2)
+            
+            ZStack{
+                VStack {
+                    ScrollView(showsIndicators: false) {
+                        HStack {
+                            Text("좋아하는 작품 모음")
+                                .font(.system(size: 22, weight: .bold))
+                            Spacer()
+                        }
                         
-                        ForEach(0..<3){_ in
-                            VStack {
-                                Image("testImage")
-                                    .resizable()
-                                    .frame(width: 160, height: 160)
-                                    .aspectRatio(contentMode: .fit)
-                                Text("작가 이름 / 작품이름")
-                                    .font(.system(size: 16, weight: .bold))
+                        ForEach(0..<4) {_ in
+                            HStack(spacing: 40) {
+                                
+                                ForEach(0..<3){_ in
+                                    VStack {
+                                        Image("testImage")
+                                            .resizable()
+                                            .frame(width: 160, height: 160)
+                                            .aspectRatio(contentMode: .fit)
+                                        Text("작가 이름 / 작품이름")
+                                            .font(.system(size: 16, weight: .bold))
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
         }
-        }
-//        .ignoresSafeArea()
-//        .padding(.horizontal, 16)
-//        .padding(.vertical, 20)
     }
 }
 
